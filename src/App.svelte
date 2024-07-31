2618a01c2a7e21048df456eec33c85cb80225fc1
Braeden Smith <braedensmith629@gmail.com>
2024-07-30 23:16:49 -0500

    Improved font size scaling

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <main>
-  <h1>👋 Hey, I'm <span class="fancy-name">Braeden</span>!</h1>
-  <p>One day, this website may be cooler. Until then, here are some links!</p>
+  <h1 class="header-text">👋 Hey, I'm <span class="fancy-name">Braeden</span>!</h1>
+  <p class="body-text">One day, this website may be cooler. Until then, here are some links!</p>
   <LinkButton href="https://github.com/braedensmith29">GitHub</LinkButton>
   <LinkButton href="https://linkedin.com/in/braedensmith29/">LinkedIn</LinkButton>
   <LinkButton href="{Resume}">Resume</LinkButton>
--- src/components/LinkButton.svelte
+++ src/components/LinkButton.svelte
@@ -2,18 +2,17 @@
   export let href;
 </script>
 
-<a class="button" {href} target="_blank"><slot /></a>
+<a class="button body-text" {href} target="_blank"><slot /></a>
 
 <style>
   .button {
-    all: unset;
+    text-decoration: none;
 
     display: inline-block;
     padding: 7px 20px;
     border: 3px solid #00b7ff;
 
     color: rgb(0, 183, 255);
-    font-size: 1.25em;
     
     cursor: pointer;
   }
